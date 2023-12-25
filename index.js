3ddb1b8238a7818ba4a5eeeaf9357923ec06b068
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2023-12-25 18:46:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const playerName = "rebekah"; 
+newHeader.textContent = `${playerName} is the champion`;
+
+document.body.appendChild(newHeader);
